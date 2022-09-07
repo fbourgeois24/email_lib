14bac9946bfd5a7d835927da204a4c428a41013f
fbourgeoi <francois.bourgeois@aiesh.be>
2022-09-07 14:42:44 +0200

    Import de ce qui est nécessaire uniquement + installation modules manquants

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
-from email_lib.email_lib import *
+import os
+
+try:
+	from email_lib.email_lib import mail_sender
+except ModuleNotFoundError:
+	# Si module non trouvé, on installe les dépendances
+	os.popen(f"pip install --no-cache-dir -r {os.path.dirname(os.path.realpath(__file__))}/requirements.txt").read()
+	from email_lib.email_lib import mail_sender
