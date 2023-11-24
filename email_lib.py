9780f228a24f9f0213d9dc0dd470943a150b3802
fbourgeoi <francois.bourgeois@aiesh.be>
2023-11-24 08:25:09 +0100

    Ajout d'un dict de config

--- email_lib.py
+++ email_lib.py
@@ -18,23 +18,66 @@ class mail_sender:
     }
 
 
-    def __init__(self, smtp_address="", smtp_port="", smtp_username="", smtp_passwd="", email_from="", autologin=True, config=None):
+    def __init__(self, smtp_address=None, smtp_port=None, smtp_username=None, smtp_passwd=None, email_from=None, reply_to=None, autologin=None, config=None):
+        """ Si une config est précisée on la lit et on l'écrase par les paramètres si précisés
+            La config peut être de 2 types (si pas None) :
+            1) str => nom de la config (voir dict configs plus haut)
+            2) dict => contient les paramètres, clés possibles :
+                - config => nom de la config (voir dict configs plus haut)
+                - addr => smtp_address
+                - port => smtp_port
+                - user => smtp_username
+                - passwd => smtp_passwd
+                - from => email_from
+                - reply_to
+                - autologin => autologin
+        """
+        # On défini les variables à None
+        self.smtp_address = None
+        self.smtp_port = None
+        self.smtp_username = None
+        self.smtp_passwd = None
+        self.email_from = None
+        self.reply_to = None
+        self.autologin = None
 
-        self.smtp_username = smtp_username
-        self.smtp_passwd = smtp_passwd
-        self.email_from = email_from
-        self.autologin = autologin
-        # Si une config est précisée on écrase le smtp_address et smtp_port
+        # Si config spécifiée, on la lit
         if config is not None:
-            if config not in self.configs:
-                raise ValueError("Invalid config name")
-            else:
+            # Si une config est spécifiée
+            if type(config) == str and config in self.configs:
                 self.smtp_address = self.configs[config]["address"]
                 self.smtp_port = self.configs[config]["port"]
-        else:
-            self.smtp_address = smtp_address
-            self.smtp_port = smtp_port
+            elif type(config) == dict:
+                # Pour addr et port, soit le nom de config est précisé dans config ou paramètre correspondant directement dans la config
+                # On vérifie que toutes les clés sont connues
+                for key in config.keys():
+                    if key not in ("config", "addr", "port", "user", "passwd", "from", "reply_to", "autologin"):
+                        raise ValueError(f"Clé '{key}' incorrecte dans la config !")
 
+                self.smtp_address = self.configs[config.get("config", {})].get("address") or config.get("addr")
+                self.smtp_port = self.configs[config.get("config", {})].get("port") or config.get("port")
+                self.smtp_username = config.get("user")
+                self.smtp_passwd = config.get("passwd")
+                self.email_from = config.get("from")
+                self.reply_to = config.get("reply_to")
+                self.autologin = config.get("autologin")
+            else:
+                raise ValueError("Invalid config name or type")
+
+       
+        # On remplace les valeurs par les paramètres si existants
+        self.smtp_address = self.smtp_address or smtp_address
+        self.smtp_port = self.smtp_port or smtp_port
+        self.smtp_username = self.smtp_username or smtp_username
+        self.smtp_passwd = self.smtp_passwd or smtp_passwd
+        self.email_from = self.email_from or email_from or self.smtp_username
+        self.reply_to = self.reply_to or reply_to or self.smtp_username
+        self.autologin = self.autologin or autologin
+        if self.autologin == None: self.autologin = True
+
+        # Si aucune valeur
+        if self.smtp_address is None or self.smtp_port is None:
+            raise ValueError("Il faut spécifier une config ou au minimum smtp_address et smtp_port")
 
     def login(self, username=None, passwd=None):
         """ Login au serveur smtp """
@@ -64,7 +107,7 @@ class mail_sender:
         self.smtp_server.quit()
 
 
-    def send_mail(self, email_to, email_subject, email_message, email_cc=[], reply_to="", files=[], content_type="html", autologin=None):
+    def send_mail(self, email_to, email_subject="", email_message="", email_from=None, email_cc=[], reply_to=None, files=[], content_type="html", autologin=None):
         """ Envoyer un mail """
         try:
             if type(email_to) == str:
@@ -72,13 +115,12 @@ class mail_sender:
             if type(email_cc) == str:
                 email_cc = (email_cc,)
             msg = MIMEMultipart()
-            msg['From'] = self.email_from
+            msg['From'] = email_from or self.email_from
             msg['To'] = COMMASPACE.join(email_to)
             msg['Cc'] = COMMASPACE.join(email_cc)
             msg['Date'] = formatdate(localtime=True)
             msg['Subject'] = email_subject
-            if reply_to != "":
-                msg.add_header('reply-to', reply_to)
+            msg.add_header('reply-to', reply_to or self.reply_to)
 
             if content_type == "text":
                 msg.attach(MIMEText(email_message))
